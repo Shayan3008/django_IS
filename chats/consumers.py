bf1b273d3cb1ba705f31874f782dbc96c8ac21ab
shayan <sb15@mmcgbl.com>
2022-11-23 16:57:53 +0500

    Configured Channels for 1-1 chats

--- chats/consumers.py
+++ chats/consumers.py
@@ -1,10 +1,25 @@
 from channels.generic.websocket import WebsocketConsumer
 import json
+from asgiref.sync import async_to_sync
 class ChatConsumer(WebsocketConsumer):
 
       def connect(self):
+          self.room_group_name = "EMAIL"
+          async_to_sync(self.channel_layer.group_add)(
+               self.room_group_name,
+               self.channel_name
+          )
           self.accept()
           self.send(json.dumps({
                "type":"Connection establish",
                "msg":"Congrats"
           }))
+      def receive(self,text_data):
+           text_data_json = json.loads(text_data)
+           message = text_data_json["message"]
+           self.send(json.dumps({
+               "type":"chat",
+               "msg":message
+          }))
+     
+          
